5f6e5b87cbe07cf1b4d62f7466a7e7ea34e13d1a
kpilszak <klaudia.pilszak@gmail.com>
2025-08-26 16:58:01 -0400

    Create boards

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const gamesBoardContainer = document.querySelector('#gamesboard-container')
 const optionContainer = document.querySelector('.option-container')
 const flipButton = document.querySelector('#flip-button')
 
@@ -8,5 +9,26 @@ function flip() {
     angle = angle === 0 ? 90 : 0
     optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`)
 }
+
+const width = 10
+
+function createBoard(color, user) {
+    const gameBoardContainer = document.createElement('div')
+    gameBoardContainer.classList.add('game-board')
+    gameBoardContainer.style.backgroundColor = color
+    gameBoardContainer.id = user
+
+    for (let i = 0; i < width * width; i++) {
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.id = i
+        gameBoardContainer.append(block)
+    }
+
+    gamesBoardContainer.append(gameBoardContainer)
+}
+
+createBoard('yellow', 'player')
+createBoard('pink', 'computer')
 
 flipButton.addEventListener('click', flip)
